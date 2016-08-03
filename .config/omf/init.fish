49cfce28d7cdf8031026e13032d5741edb91d8be
larribas <lorenzo.s.arribas@gmail.com>
2016-08-03 08:28:18 +0200

    Fix omf and git configs

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,13 +1,13 @@
 # DEFAULT EDITOR
-export EDITOR='vim'
+set -gx EDITOR 'vim'
 
 # HOMEBREW
-export PATH=$PATH:/usr/local/sbin
+set -gx PATH $PATH "/usr/local/sbin"
 
 # RBENV
 status --is-interactive; and . (rbenv init -|psub)
 
 # GOLANG
-export GOPATH=~/Dev/go
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+set -gx GOPATH "~/Dev/go"
+set -gx PATH $PATH "/usr/local/go/bin" "$GOPATH/bin"
 
