b8399429268b0e595ea21dededcc0e19057ade38
larribas <lorenzo.s.arribas@gmail.com>
2016-09-20 11:31:34 +0200

    Add ssh-agent to fish initialization

---
+++ fish/.config/fish/functions/mcd.fish
@@ -0,0 +1,4 @@
+function mcd -d "mkdir and cd into a new dir"
+  mkdir -p $argv[1]
+  cd $argv[1]
+end
--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -5,6 +5,14 @@
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 
+# SSH Agent
+if test -z "$SSH_ENV"
+    setenv SSH_ENV $HOME/.ssh/environment
+end
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
+
 # Ruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
