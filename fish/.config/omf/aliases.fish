a16eaeb8a489689d7ce97c3aa6abcb62dcb7b876
larribas <lorenzo.s.arribas@gmail.com>
2017-05-28 10:06:25 +0200

    Add aliases for pbcopy for linux

--- fish/.config/omf/aliases.fish
+++ fish/.config/omf/aliases.fish
@@ -6,4 +6,14 @@ abbr -a e='eval $EDITOR'
 abbr -a bi='bundle install'
 abbr -a be='bundle exec'
 
+# Pbcopy on linux
+switch (uname)
+case Darwin 
+  abbr -a cpkey='pbcopy < ~/.ssh/id_rsa.pub'
+case Linux
+  abbr -a pbcopy='xclip -sel clip'
+  abbr -a cpkey='xclip -sel clip < ~/.ssh/id_rsa.pub'
+end
+
+
 
