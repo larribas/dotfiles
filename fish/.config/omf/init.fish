16a0806a542d11cf7ae464091c0e0ab20f6d1bb2
larribas <lorenzo.s.arribas@gmail.com>
2018-09-12 10:45:03 +0200

    Remove dependencies between fish and language-specific environments

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -12,21 +12,5 @@ end
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 
-# Ruby
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
-
-# Go
-set -gx GOPATH ~/Dev/go
-set -gx GOBIN ~/Dev/go/bin
-set -gx PATH $GOBIN /usr/local/go/bin $PATH
-
-# Haskell
-set -gx PATH $PATH /home/larribas/.local/bin
-
-# gcloud
-set fish_user_paths /Users/loa/google-cloud-sdk/bin
-set -x MANPATH /Users/loa/google-cloud-sdk/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
-
 # Javascript
 set -gx PATH ./node_modules/.bin $HOME/.config/yarn/global/node_modules/.bin $PATH
