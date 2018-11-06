73deddcd3452f58c2d0255471d467470eda8232a
larribas <lorenzo.s.arribas@gmail.com>
2018-11-06 17:59:34 +0100

    Add spacemacs configuration

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -1,3 +1,5 @@
+set fish_greeting ""
+
 . ~/.config/omf/aliases.fish
 
 if test -e ~/.config/omf/exclusive_aliases.fish
@@ -9,8 +11,18 @@ end
 
 
 # Main editor
-set -gx VISUAL nvim
-set -gx EDITOR nvim
+set -gx VISUAL emacs
+set -gx EDITOR emacs
 
 # Javascript
 set -gx PATH ./node_modules/.bin $HOME/.config/yarn/global/node_modules/.bin $PATH
+
+# emacs ansi-term support
+if test -n "$EMACS"
+  set -x TERM eterm-color
+end
+
+# this function may be required
+function fish_title
+  true
+end
